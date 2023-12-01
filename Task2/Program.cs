14a86a11fba97e01e0c9434f16bdcf38c28b9080
AliNazarovv <alirn@code.edu.az>
2023-12-01 15:21:32 +0400

    Task2 cvb

---
+++ Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 ci tapsiriq
+            string name = "Ali";
+            string surname = "Nazarov";
+            string fullname = name  + surname;
+            Console.WriteLine($" Sexsin melumatlari  {fullname} ");
+            //2 ci tapsiriq
+            int a = 6;
+            int b = 7;
+            Console.WriteLine($"Ededin cemi : {a+b}");
+            //3 cu tapsiriq
+            int x = 8;
+            int y = 9;
+            Console.WriteLine($"Ededin ferqi : {x - y}");
+            //4 cu tapsiriq
+            int z = 4;
+            int v = 5;
+            int resultt = z * v;
+            Console.WriteLine($"Ededin hasili : {resultt*resultt}");
+
+            //5 ci tapsiriq
+            int c = 10;
+            int d = 5;
+            int result = c/d;
+            Console.WriteLine($" Ededin bolmesi : {result*result}");
+        }
+    }
+}
